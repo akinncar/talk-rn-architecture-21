261b48240b2e83c73f6e6cdd8415ee177ea0d1a2
akinncar <akinncar@hotmail.com>
2021-09-14 20:54:57 -0300

    feat: init

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { dark as theme } from "mdx-deck";
+
+export default {
+  ...theme,
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+};
